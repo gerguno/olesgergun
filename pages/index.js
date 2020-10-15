fc1bd0d295ba82c0bf226a24619cde25b6ee5100
olesgergun <gerguno@gmail.com>
2020-10-15 16:15:15 +0300

    yeah

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
 export default function Index() {
-	return <h1>Hello</h1>
+	return (
+		<>
+			<Head>
+				<title>Oleś Gergun</title>
+				<meta name="keywords" content='oles, gergun, ui, design, type, typography, code'/>
+				<meta name="description" content='oles, gergun, ui, design, type, typography, code'/>
+				<meta charSet="utf-8"/>
+			</Head>
+
+			<h1>Index page</h1>
+			<p>Lorem ipsum</p>
+
+			<p><Link href={'/about'}><a>About</a></Link></p>
+			<p><Link href={'/texts'}><a>Texts</a></Link></p>
+		</>
+	)
 }
