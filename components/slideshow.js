a4f3ae8000b3f03c9b3dfe137b32e851ae49999e
olesgergun <gerguno@gmail.com>
2020-10-30 15:02:19 +0200

    carousel fade done!

--- components/slideshow.js
+++ components/slideshow.js
@@ -1,32 +1,13 @@
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import FadeCarousel from './FadeCarousel'
+
 
 export function SlideShow({media, color}){
     let isColor = color.indexOf('#') > -1
-    const settings = {
-        autoplay: true,
-        dots: false,
-        arrows: false,
-        fade: true,
-        infinite: true,
-        focusOnSelect: false,
-        speed: 1000,
-        slidesToShow: 1,
-        swipe: false,
-        touchMove: false
-    }
     return (
         <>
             <div className="slideshow" style={isColor ? {backgroundColor:color} : {}}>
                 <div className="screen-desktop">
-                    <Slider {...settings}>
-                        {media.map((medium) => (
-                            <div>
-                                <img src={medium.url}/>
-                            </div>
-                        ))}
-                    </Slider>
+                    <FadeCarousel media={media}/>
                 </div>
             </div>
         </>
