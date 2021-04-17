ec3d3b7d119db838b6bd8955b01f15e5a15d1b6a
Oles Gergun <gerguno@gmail.com>
2021-04-17 21:12:47 +0300

    slug mobile+

--- components/SuperMedium.js
+++ components/SuperMedium.js
@@ -15,7 +15,7 @@ export default function SuperMedium({ full, deviceType, deviceMedia, backgroundC
             {(backgroundMedium && (backgroundMedium.mimeType === "video/quicktime" || backgroundMedium.mimeType === "video/mp4"))
                 ?
                     <div className={`supermedium ${full ? `__full` : `__half`}`}>
-                        <video src={backgroundMedium.url} type={backgroundMedium.mimeType} loop autoPlay muted/>
+                        <video src={backgroundMedium.url} type={backgroundMedium.mimeType} loop autoPlay muted playsinline/>
                     </div>
                 :
                     width > 768
