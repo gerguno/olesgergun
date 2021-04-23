016fbee715da8460d9ffb712bc01db6edd1ac05c
Oles Gergun <gerguno@gmail.com>
2021-04-23 20:54:00 +0300

    homepage +-

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -7,51 +7,6 @@ import useWindowDimensions from "./useWindowDimensions";
 import { useRouter } from 'next/router'
 
 export function MainLayout({children, title='Oleś Gergun'}) {
-    const [clicked, setClicked] = useState(false)
-    const [pushed, setPushed] = useState(false)
-    const [aboutBlack, setAboutBlack] = useState(false)
-
-    const { height, width } = useWindowDimensions()
-
-    // const push = () => {
-    //     !clicked && setClicked(true)
-    //     setPushed(true)
-    //
-    //     let nav = document.querySelector('nav')
-    //     nav.className = "__pushed"
-    // }
-    //
-    // const unpush = () => {
-    //     setPushed(false)
-    //     let nav = document.querySelector('nav')
-    //     nav.className = ""
-    //
-    //     let black = document.querySelector('about').lastChild
-    //     if (aboutBlack) {
-    //         black.className = "black fadeOut"
-    //         setAboutBlack(false)
-    //     }
-    // }
-    //
-    // const toggleAboutBackground = () => {
-    //     setAboutBlack(!aboutBlack)
-    // }
-    // useEffect(() => {
-    //     let black = document.querySelector('about').lastChild
-    //     if (clicked) {
-    //         aboutBlack ? black.className = "black fadeIn" : black.className = "black fadeOut"
-    //     }
-    //
-    //     let rightTitles = document.querySelectorAll('.right-title')
-    //     Array.from(rightTitles).map(rt => {
-    //         if (aboutBlack) {
-    //             rt.style.color = '#676767'
-    //         } else {
-    //             rt.style.color = '#b2b2b2'
-    //         }
-    //     })
-    // }, [aboutBlack])
-
     return (
         <>
             <Head>
@@ -60,9 +15,7 @@ export function MainLayout({children, title='Oleś Gergun'}) {
                 <meta name="description" content='oles, gergun, ui, design, type, typography, code'/>
                 <meta charSet="utf-8"/>
             </Head>
-            {width > 767 ? <Menu/> : <MenuMobile/>}
-
-            <main className={pushed ? '__pushed': ''}>
+            <main>
                 {children}
             </main>
         </>
--- pages/index.js
+++ pages/index.js
@@ -18,36 +18,64 @@ export default function Index({ posts }) {
 
 	return (
 		<MainLayout title={'Workbench'}>
-			<div className="home-projects">
-				{posts.map(post => (
-					<Link href={`/work/[slug]`} as={`/work/${post.slug}`}>
-						<a>
+			<div className="rainbow">
+				<div className="left">
+					<div className="logo">
+						Oleś Gergun
+					</div>
+					<div className="contacts">
+						Email: <a href="mailto:hello@olesgergun.com">hello@olesgergun.com</a> <br/>
+						Social: <a href="https://www.instagram.com/olesgergun">Instagram</a>, <a href="https://www.facebook.com/olesgergun">Facebook</a>, <a href="https://github.com/gerguno">GitHub</a>, <br/>
+						<a href="https://www.instagram.com/kyiv_type_digest">Kyiv Type Digest</a>, <a href="https://www.instagram.com/kyiv_type_foundry">Kyiv Type Foundry</a> <br/>
+						© 2021 All rights reserved
+					</div>
+				</div>
+				<div className="right">
+					<img src="/oi.png"/>
+				</div>
+			</div>
+			<section className="container">
+				<div className="motto">
+					Architecting
+					{width < 768 ? <br/> : ' '}
+					the digital
+				</div>
+				{width < 960 &&
+				<div className="home-intro">
+					<h2>
+						Oleś Gergun is a digital designer and developer based in Kyiv. He has expertise in a wide range of practices with horizontal deepening into digital products сreation
+					</h2>
+					<div className="read-about">
+						<Link href={`/about`}>
+							<a>
+								Read about
+							</a>
+						</Link>
+					</div>
+				</div>
+				}
+				<section className="home">
+					<div className="home-projects">
+						{posts.map(post => (
 							<div className="home-project">
-								<div className="home-project-title">
-									<div className="home-title">
+								<Link href={`/work/[slug]`} as={`/work/${post.slug}`}>
+									<a className="home-project-title">
 										<img src="/bullet.svg"/>
 										{post.title}
-										<span className="grey">
-										{width < 576 ? <br/> : <>&nbsp;</>}
+										<span className="dark-grey">
+											{width > 375
+												?
+													' '
+												:
+													post.title.length > 7 ? <br/> : ' '
+											}
 											{post.afterTitle}
-										</span>
-									</div>
-									{width > 1366 &&
-										<div className="tags">
-											{post.tags.split(',').map(t => {
-												return (
-													<div className="tag">
-														{t}
-													</div>
-												)
-											})}
-										</div>
-									}
-								</div>
+									</span>
+									</a>
+								</Link>
 								<div className="home-project-description">
 									<h1>{post.shortDescription}</h1>
 								</div>
-								{width < 1366 &&
 								<div className="tags">
 									{post.tags.split(',').map(t => {
 										return (
@@ -57,14 +85,34 @@ export default function Index({ posts }) {
 										)
 									})}
 								</div>
-								}
 							</div>
-						</a>
-					</Link>
-				))}
-			</div>
-			<Footer/>
-			<div className="rainbow"></div>
+						))}
+					</div>
+					{width > 959 &&
+					<div className="home-intro">
+						<h2>
+							Oleś Gergun is a digital designer and developer based in Kyiv. He has expertise in a wide range of practices with horizontal deepening into digital products сreation:
+							<br/><br/>
+							(1) User experience design<br/>
+							Big fan of strategy, creating user flows, algorithms, new UI patterns as well as replicating conventional ones, generating tone of voice and design language.
+							<br/><br/>
+							(2) Web development<br/>
+							HTML, CSS (incl. preprocessors Sass, Less), advanced JavaScript (ES5, ES6+), RestAPI, GraphQL, ReactJS (Context API, React hooks), NextJS, Node.js (basics, incl. Express.js)
+							<br/><br/>
+							(3) Type design<br/>
+							Paying attention to type as fundamental to the design. Exploring type forms of the past as well as of today’s street visual culture (predominantly of Eastern Europe origin).
+						</h2>
+						<div className="read-about">
+							<Link href={`/about`}>
+								<a>
+									Read about
+								</a>
+							</Link>
+						</div>
+					</div>
+					}
+				</section>
+			</section>
 		</MainLayout>
 	)
 }
